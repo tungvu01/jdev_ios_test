957d7d76e99f1f994f537c81a8dd49dc1ff49418
Tung Vu <tungvt@vnext.com.vn>
2021-12-02 13:59:30 +0700

    init project

---
+++ JDevTask/JDevTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JDevTask
+//
+//  Created by Tung Vu on 02/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
